2945a9df83add9556594a2795dc8d8569aea5738
Judare85 <judare85@hotmail.com>
2023-05-25 22:15:25 -0500

    fourth commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./portfolio.css";
 import Pokedex from "../../assets/Pokedex.jpg";
+import Ecommerce from "../../assets/Ecommerce.png";
 
 const Portfolio = () => {
   return (
@@ -13,27 +14,45 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={Pokedex} alt="" />
           </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="" className="btn" target="_blank">
+          <h3>POKEAPI</h3>
+          <a
+            href="https://github.com/Judare1990/Entregable5"
+            className="btn"
+            target="_blank"
+          >
             GITHUB
           </a>
-          <a href="" className="btn btn-primary" target="_blank">
+          <a
+            href="https://dazzling-fenglisu-939c6b.netlify.app/#/pokedex/2"
+            className="btn btn-primary"
+            target="_blank"
+          >
             Live Demo
           </a>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image"></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="" className="btn" target="_blank">
+          <img src={Ecommerce} alt="" />
+          <h3>ECOMMERCE</h3>
+          <a
+            href="https://github.com/Judare1990/Ecommerce1"
+            className="btn"
+            target="_blank"
+          >
             GITHUB
           </a>
-          <a href="" className="btn btn-primary" target="_blank">
+          <a
+            href="https://melodious-pasca-979bce.netlify.app/"
+            className="btn btn-primary"
+            target="_blank"
+          >
             Live Demo
           </a>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image"></div>
-          <h3>This is a portfolio item title</h3>
+          <img src={Ecommerce} alt="" />
+          <h3>RICK AND MORTY</h3>
           <a href="" className="btn" target="_blank">
             GITHUB
           </a>
