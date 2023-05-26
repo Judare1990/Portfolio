c5126894b4f58d1a443a43235ddcb3b7f0682bb6
Judare85 <judare85@hotmail.com>
2023-05-26 16:39:46 -0500

    last commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,12 +15,13 @@ const About = () => {
           </div>
         </div>
         <p className="about__me-content">
-          I am a full-stack web developer who loves what I do. I stand out for
-          my teamwork and my perseverance; I like to meet the goals set and
-          learn from each project in order to grow up personally and
-          professionally every day. I always try to find the best way to solve
-          issues that we can face up at work. I am willing to learn new
-          programming languages.
+          I am a full-stack web developer who is passionate about programming,
+          conflict resolution and teamwork, based on discipline and
+          perseverance. I stand out for my adaptability to change. Furthermore,
+          i like to meet the goals set and learn from each project in order to
+          grow up personally and professionally every day. I always try to find
+          the best way to solve issues that we can face up at work. I am willing
+          to learn new programming languages.
           <a href="#contact" className="btn btn-primary">
             Let's Talk
           </a>
